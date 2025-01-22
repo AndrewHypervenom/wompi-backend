b6974592ffbd7d4489d15c21e28c9177c80a95e9
darkshark-ctrl <andrewgpt721@gmail.com>
2025-01-22 00:26:12 -0500

    fix: Corregidas rutas de importación para el servicio de transacciones

--- src/routes/transaccionRoutes.js
+++ src/routes/transaccionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createTransaction } = require('../services/transactionService');
+const { createTransaction } = require('../services/transactionService.js');
 
 router.post('/', async (req, res) => {
   try {
